2bfbdde69cad1a19d325b6879b0ed6f5adcc91c9
Stefan Schultz <28778796+itsmestefanjay@users.noreply.github.com>
2021-12-10 14:45:19 +0100

    update dependencies

--- src/main/java/de/stefanjay/delegate/SayHelloDelegate.java
+++ src/main/java/de/stefanjay/delegate/SayHelloDelegate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Stefan Schultz
+ * Copyright (C) 2021 Stefan Schultz
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
